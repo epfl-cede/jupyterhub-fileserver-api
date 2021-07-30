824e6666002d04cdc733f52251311bc89620ff3a
bgiger <wawotsch@gmail.com>
2021-07-30 16:23:07 +0200

    Replace special characters in username (@, .)

--- libs/dummyUser.py
+++ libs/dummyUser.py
@@ -1,6 +1,7 @@
 class notoUser:
     """
-    Mockup class to fake the EPFL notouser library. For a given ID, just returns this ID.
+    Mockup class to fake the EPFL notouser library. For a given ID, just returns this ID, after doing some
+    sanitizing (hex encoding of special characters in mail address).
     """
 
     def __init__(self):
@@ -14,5 +15,7 @@ class notoUser:
         :param id: THE id, user identification from IAM
         :param email: 2nd argument for validation?
         """
+        id = id.replace(".", "-2e")
+        id = id.replace("@", "-40")
         user = {"normalised": id, "uid": id}
         return user
